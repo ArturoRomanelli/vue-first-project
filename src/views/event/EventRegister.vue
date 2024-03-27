b08cb36dd16181f8b0c183d14f9497ed8a594bf0
Arturo Romanelli <arturo.romanelli@wde.it>
2024-03-27 11:33:32 +0100

    Added `RegisterMe` button that redirects to details

--- src/views/event/EventRegister.vue
+++ src/views/event/EventRegister.vue
@@ -1,9 +1,15 @@
 <template>
   <p>Register for the event here</p>
+  <button @click="register">Register me</button>
 </template>
 
 <script>
 export default {
-  props: ['event']
+  props: ['event'],
+  methods: {
+    register() {
+      this.$router.push({ name: 'EventDetails', params: { id: this.event.id } })
+    }
+  }
 }
 </script>
